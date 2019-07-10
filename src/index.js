bfa28b5da98e6e3ce59b57db5ba87addcee60a57
Tim Vlasuk <timvlasuk@gmail.com>
2019-07-10 13:38:07 -0700

    going through 'asset management' guide from webPack site: changing names and adding rules to config file to load css/styles and content/images/font

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import _ from 'lodash';
+import './style.css';
+import Proper from './proper.gif';
 
 function component() {
 	const element = document.createElement('div');
 
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+	element.classList.add('hello');
+
+	const myGif = new Image();
+	myGif.src = Proper;
+
+	element.appendChild(myGif);
 
 	return element;
 }
