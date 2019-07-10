5e2a91569e4259f614dd0d7c578560530d8199fe
Tim Vlasuk <timvlasuk@gmail.com>
2019-07-10 14:51:20 -0700

    final instructions to clean up example project for next tutorial

--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
 import _ from 'lodash';
-import './style.css';
-import Proper from './proper.gif';
-import Data from './data.xml';
 
 
 function component() {
 	const element = document.createElement('div');
 
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-	element.classList.add('hello');
 
-	const myGif = new Image();
-	myGif.src = Proper;
 
-	element.appendChild(myGif);
-
-	console.log(Data)
-	
 	return element;
 }
 
