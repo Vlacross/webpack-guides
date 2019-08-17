a910f07e64642c5fe9221d9c84486bdd0eead7aa
Tim Vlasuk <timvlasuk@gmail.com>
2019-08-17 13:43:04 -0700

    installed clean-webpack-plugin which seems to remove any unreferenced files

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 
 module.exports = {
   entry: {
@@ -8,6 +10,7 @@ module.exports = {
     print: './src/print.js'
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Output Management'
     })
