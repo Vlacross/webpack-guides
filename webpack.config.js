17d0e1122d964ba80d6647b193ad37dc7c31c0ee
Tim Vlasuk <timvlasuk@gmail.com>
2019-08-20 15:44:28 -0700

    altered index.js to dynamically import lodash and pass element through promise syntax flow control

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    app: './src/index.js',
-    another: './src/another-module.js'
+  index: './src/index.js'
     // print: './src/print.js'
   },
   devtool: 'inline-source-map',
@@ -24,13 +23,15 @@ module.exports = {
   ],
   output: {
     filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/'
-  },
-  optimization: {
-    splitChunks: {
-      chunks: 'all'
-    }
   }
+  // ,
+  // optimization: {
+  //   splitChunks: {
+  //     chunks: 'all'
+  //   }
+  // }
  
 };
